ec75f5a9ca1d7873255fcfcb2deab41a82e0b0f9
지승 <rjs8833@gmail.com>
2025-02-02 21:49:59 +0900

    apply pretendard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-	return <div className="flex h-screen w-screen flex-col justify-between bg-gray-100"></div>;
+	return (
+		<div className="flex h-screen w-screen flex-col justify-between bg-gray-100 text-black">
+			안녕
+		</div>
+	);
 }
 
 export default App;
